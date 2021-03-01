44f164ac9bd195bc948e103361ad5912e0e7c6a7
Louis Gombert <louisgombert@gmail.com>
2021-03-01 11:02:28 +0100

    test deploy on master

--- mobi/views.py
+++ mobi/views.py
@@ -4,4 +4,4 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def health_check(request):
-    return Response({'status': 'ok', 'message': 'ça maaarche'})
+    return Response({'status': 'ok', 'message': 'ça maarche'})
