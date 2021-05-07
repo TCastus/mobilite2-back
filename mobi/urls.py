8927a356b8f0f40df0432d6599b6f5b659eb172c
ghashrod <bastien.fontaine18520@gmail.com>
2021-05-07 13:21:24 +0200

    WIP

--- mobi/urls.py
+++ mobi/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import health_check, ReviewViewset, CountryViewset, UniversityViewset
+from .views import health_check, ReviewViewset, CountryViewset, UniversityViewset, search
 
 # Register models to the REST router
 router = routers.DefaultRouter()
@@ -12,5 +12,6 @@ router.register(r'university', UniversityViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('check', health_check, name="healthcheck")
+    path('check', health_check, name="healthcheck"),
+    path('search', search, name='search')
 ]
--- mobi/views.py
+++ mobi/views.py
@@ -31,3 +31,32 @@ class UniversityViewset(viewsets.ModelViewSet):
     queryset = University.objects.all()
     serializer_class = UniversitySerializer
 
+
+@api_view(['POST'])
+def search(request):
+    if request.method == 'POST':
+
+        queryset = University.objects.all()
+
+        uni_name = request.data['name']
+        uni_access = request.POST.get("access")
+        uni_cultural_life_min = request.POST.get("cultural_life_min")
+        uni_country = request.POST.get("country")
+        uny_nightlife = request.POST.get("nightlife")
+        uni_security = request.POST.get("security")
+        uni_rent_max = request.POST.get("rent_max")
+        uni_courses_diff = request.POST.get("courses_difficulty")
+        uni_cost_living = request.POST.get("cost_living")
+        uni_outside_europe = request.POST.get("outside_europe")
+
+        queryset = queryset.filter(
+            name=uni_name,
+        )
+
+        print(request.POST.get("name"))
+
+        serializer = UniversitySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
