bf21dc221d7b007d42c17920d6c5bb64222e2184
ghashrod <bastien.fontaine18520@gmail.com>
2021-05-18 14:49:14 +0200

    Je crois qu'on est encore mieux

--- mobi/views.py
+++ mobi/views.py
@@ -27,7 +27,7 @@ class UniversityViewset(viewsets.ModelViewSet):
                         'courses_difficulty',
                         'student_proximity',
                         'name', )
-    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    filter_backends = (filters.SearchFilter)
     queryset = University.objects.all()
     serializer_class = UniversitySerializer
 
@@ -40,37 +40,34 @@ def search(request):
 
         if 'name' in request.data:
             uni_name = request.data['name']
-            queryset = queryset.filter(name__iexact=uni_name)
+            queryset = queryset.filter(name__icontains=uni_name)
         if 'access' in request.data:
             uni_access = request.data['access']
             queryset = queryset.filter(access=uni_access)
         if 'country' in request.data:
             uni_country = request.data['country']
             queryset = queryset.filter(city__country__name__icontains=uni_country)
-        if 'nightlife' in request.data:
-            uni_nightlife = request.data['nightlife']
-            queryset = queryset.filter(city__night_life_average_grade__gte=uni_nightlife)
+        if 'nightlife_min' in request.data:
+            uni_nightlife_min = request.data['nightlife_min']
+            queryset = queryset.filter(city__night_life_average_grade__gte=uni_nightlife_min)
         if 'courses_diff' in request.data:
             uni_courses_diff = request.data['courses_difficulty']
             queryset = queryset.filter(courses_difficulty__lte=uni_courses_diff)
-        if 'security' in request.data:
-            uni_security = request.data['security']
-            queryset = queryset.filter(city__security_average_grade__gte=uni_security)
-        if 'cultural_life_min' in request.data:
-            uni_cultural_life_min = request.data['cultural_life_min']
-            queryset = queryset.filter(city__cultural_life_average_grade__gte=uni_cultural_life_min)
+        if 'cultural_min' in request.data:
+            uni_cultural_min = request.data['cultural_min']
+            queryset = queryset.filter(city__cultural_life_average_grade__gte=uni_cultural_min)
         if 'outside_europe' in request.data:
             uni_outside_europe = request.data['outside_europe']
             if uni_outside_europe == 'True':
                 queryset = queryset.exclude(city__country__continent="Europe")
             else:
                 queryset = queryset.filter(city__country__continent="Europe")
-        if 'rent_max' in request.data:
-            uni_rent_max = request.data['rent_max']
-            queryset = queryset.filter(rent_average__lte=uni_rent_max)
-        if 'cost_living_max' in request.data:
-            uni_cost_living_max = request.data['cost_living_max']
-            queryset = queryset.filter(city__cost_of_living_average_grade__lte=uni_cost_living_max)
+        if 'department_availability' in request.data:
+            uni_department_availability = request.data['department_availability']
+            queryset = queryset.filter(department_availability__iexact=uni_department_availability)
+        if 'cost_living_grade_min' in request.data:
+            uni_cost_living_grade_min = request.data['cost_living_grade_min']
+            queryset = queryset.filter(city__cost_of_living_average_grade__gte=uni_cost_living_grade_min)
 
         serializer = UniversitySerializer(queryset, many=True)
 
