23f25334cb13e2cb3e5fac4534e7ff62749d2723
T-lorrain <72496171+T-lorrain@users.noreply.github.com>
2021-06-03 10:47:15 +0200

    Gestion module Import Export Data CSV

--- mobi/admin.py
+++ mobi/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
-
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 from mobi.models import University, ExchangeReview, City, DepartementINSA, Country, Semester, FinancialAid, PlacesExchange, PlacesDD
 
-admin.site.register(University)
 admin.site.register(ExchangeReview)
-admin.site.register(City)
 admin.site.register(DepartementINSA)
-admin.site.register(Country)
+
+
+@admin.register(Country)
+class CountryAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(City)
+class CityAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(University)
+class UniversityAdmin(ImportExportModelAdmin):
+    pass
+
+
 admin.site.register(Semester)
 admin.site.register(FinancialAid)
 admin.site.register(PlacesExchange)
